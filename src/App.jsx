06c2d141fb9a2b3c37f3f179405275beb364a60c
Prathamesh Santosh Deshmukh <prathameshsantoshdeshmukh123@gamil.com>
2025-07-20 02:44:13 +0530

    Merged context API with Reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
 import AddTodo from "./Components/AddTodo/AddTodo";
 import TodoList from "./Components/Todo-List/Todo-List";
 import TodoContext from "./Context/TodoContext";
+import TodoReducers from "./Reducers/TodoReducers";
+import TodoDispatchContext from "./Context/TodoDispatchContext";
 
 function App() {
-  const [list, setList] = useState([
-    { id: 1, todoData: "todo 1", finished: false },
-    { id: 2, todoData: "todo 2", finished: true },
-  ]);
+  // const [list, setList] = useState([
+  //   { id: 1, todoData: "todo 1", finished: false },
+  //   { id: 2, todoData: "todo 2", finished: true },
+  // ]);
+  const [list, dispatch] = useReducer(TodoReducers, []);
   return (
-    <TodoContext.Provider value={{list,setList}}>
-      <h1>Todo App</h1>
-      <AddTodo
-        updateList={(todo) =>
-          setList([
-            ...list,
-            { id: list.length + 1, todoData: todo, finished: false },
-          ])
-        }
-      />
-      <TodoList />
+    <TodoContext.Provider value={{ list }}>
+      <TodoDispatchContext.Provider value={{dispatch}}>
+        <h1>Todo App</h1>
+        <AddTodo />
+        <TodoList />
+      </TodoDispatchContext.Provider>
     </TodoContext.Provider>
   );
 }
