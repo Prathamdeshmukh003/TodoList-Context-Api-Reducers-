2a1f3c3f553e68c24a4dc89a7d6dc974a80e9512
Prathamesh Santosh Deshmukh <prathameshsantoshdeshmukh123@gamil.com>
2025-07-20 19:35:32 +0530

    Enter functionality to input tag

--- src/Components/AddTodo/AddTodo.jsx
+++ src/Components/AddTodo/AddTodo.jsx
@@ -1,10 +1,20 @@
 import { useContext, useState } from "react";
 import TodoDispatchContext from "../../Context/TodoDispatchContext";
 import './AddTodo.css';
+import Todo from "../Todo/Todo";
 
 function AddTodo(){
     const [inputText,setInputText] = useState('');
     const {dispatch} = useContext(TodoDispatchContext);
+    const handleDownKey = (e)=>{
+        if(e.key === "Enter"){
+            e.preventDefault();
+            if(inputText.trim() !== ""){
+                dispatch({type : 'add_todo',payload : {todoText : inputText}})
+                setInputText('');
+            }
+        }
+    }
     return (
         <div className="AddTodo">
             <input 
@@ -13,6 +23,7 @@ function AddTodo(){
                 type="text"
                 value={inputText}
                 onChange={e=> setInputText(e.target.value)}
+                onKeyDown={handleDownKey}
             />
             <button className="Add" onClick={()=>{
                 dispatch({type : 'add_todo',payload : {todoText : inputText}})
